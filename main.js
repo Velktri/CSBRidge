9b059237163791babd87a353fb7c77c6fb5efd32
Velktri <murrayg226@gmail.com>
2021-01-19 11:37:55 -0800

    updates

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let hostName = 'velktri.github.io'
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color: '#3aa757' }, () => {
         console.log("The color is green!")
@@ -6,7 +8,7 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
+                pageUrl: { hostEquals: hostName },
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }])
