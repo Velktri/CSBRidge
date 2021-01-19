a7f301cbbeb6e1d1b7b5f62052072f1f34501b4b
Velktri <murrayg226@gmail.com>
2021-01-19 11:53:54 -0800

    fixed onclick bug

--- testing/scanner.js
+++ testing/scanner.js
@@ -1,5 +1,5 @@
-scanner.onclick(() => {
+scanner.onclick = () => {
     document.getElementById(name).innerHTML = "John Doe"
     document.getElementById(address).innerHTML = "123 Main ST apt 1"
     document.getElementById(zipcode).innerHTML = "1234567"
-})
+}
