adf1d10db2ec6b82734f46b5151499f42e879687
Velktri <murrayg226@gmail.com>
2021-02-14 10:49:54 -0800

    update destination

--- destination_content.js
+++ destination_content.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action == "sendToDestination")
     {
-        console.log(message.data)
+        document.getElementById('input__address').innerHTML = message.data.addr1
+        document.getElementById('input__city').innerHTML = message.data.addr2
+        document.getElementById('input__zip').innerHTML = message.data.zip
     }
 })
